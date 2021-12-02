1c50f7fe1c9c12ae0c0d75eed075042241f9bb71
Al Green <a.l.green@hotmail.com>
2021-12-01 21:54:40 -0600

    Team commit - add delete route for testing post

--- routes/posts.js
+++ routes/posts.js
@@ -1,22 +1,21 @@
 const { Post, validatePost } = require("../models/postSchema");
-const { User, validateUser } = require("../models/userSchema"); 
-const express = require('express');
+const { User, validateUser } = require("../models/userSchema");
+const express = require("express");
 const router = express.Router();
 
+router.get("/:id/posts", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post)
+      return res
+        .status(400)
+        .send(`The post with id "${req.params.id}" does not exist`);
 
-router.get('/:id/posts', async (req, res) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (!post)
-            return res.status(400).send(`The post with id "${req.params.id}" does not exist`);
-
-            return res.send(post);
-
-    } catch (ex) {
-        return res.status(500).send(`Internal Server Error: ${ex}`)
-    }
-})
-
+    return res.send(post);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
 router.post("/:id/posts", async (req, res) => {
   try {
@@ -39,50 +38,68 @@ router.post("/:id/posts", async (req, res) => {
   }
 });
 
-router.put('/:id/posts/:id', async (req, res) => {
-    try {
+router.put("/:id/posts/:id", async (req, res) => {
+  try {
+    const { error } = validatePost(req.body);
+    if (error) return res.status(400).send(error);
 
-        const { error } = validatePost(req.body);
-        if (error) return res.status(400).send(error);
-        
-        const post = await Post.findByIdAndUpdate(
-            req.params.id,
-            {
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: req.body.password,
-                dob: req.body.dob
-            },
-            { new: true} 
-            );
-            if (!post)
-                return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
+    const post = await Post.findByIdAndUpdate(
+      req.params.id,
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        dob: req.body.dob,
+      },
+      { new: true }
+    );
+    if (!post)
+      return res
+        .status(400)
+        .send(`The post with id "${req.params.id}" does not exist.`);
 
-                await post.save();
+    await post.save();
 
-                return res.send(post);
+    return res.send(post);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
-        } catch (ex) {
-            return res.status(500).send(`Internal Server Error: ${ex}`);
-        }
-        
-})
 
-router.delete('/:id', async (req, res) => {
-    try {
-        
-        const post = await Post.findByIdAndDelete(req.params.id);
 
-        if (!post)
-            return res.status(400).send(`The post with id "${req.params.id}" does not exist.`);
 
-            return res.send(post);
 
-        } catch (ex) {
-            return res.status(500).send(`Internal Server Error: ${ex}`);
 
-    }
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router.delete("/:id/posts/:postid", async (req, res) => {
+//   try {
+//     const post = await Post.findByIdAndRemove(req.params.postid);
+
+//     if (!post)
+//       return res
+//         .status(400)
+//         .send(`The user with id "${req.params.id}" does not exist.`);
+
+
+//     return res.send(post);
+//   } catch (ex) {
+//     return res.status(500).send(`Internal Server Error: ${ex}`);
+//   }
+// });
+
 
 module.exports = router;
